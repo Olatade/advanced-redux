0340318df52748f7eec78eab4be038993e3a93f3
Olatade <4tade01@gmail.com>
2024-07-23 14:17:18 -0700

    thunks without normalizing data

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
+})
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+
+  // works with createAsyncThunk
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // Immer lets us update state in two ways: either mutating the existing state value, or returning a new result. If we return a new value, that will replace the existing state completely with whatever we return.
+      // whatever we return will be the new state
+      return action.payload
+    })
+  },
+})
+
+export default usersSlice.reducer
